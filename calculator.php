d2577fe590d9e9270413fc486de2bef6551fd0cf
dikotofeel <julianavalidor@gmail.com>
2025-08-27 17:08:23 +0800

    Add files via upload

--- calculator.php
+++ calculator.php
@@ -9,37 +9,46 @@
 ini_set('display_errors', 0);
 $result = "";
 
+// Define operation functions
+function add($a, $b) { return $a + $b; }
+function subtract($a, $b) { return $a - $b; }
+function multiply($a, $b) { return $a * $b; }
+function divide($a, $b) { return $b != 0 ? $a / $b : "Cannot divide by 0"; }
+function modulo($a, $b) { return $b != 0 ? $a % $b : "Cannot modulo by 0"; }
+
+// Map button names to function names
+$operations = [
+    'add' => 'add',
+    'subtract' => 'subtract',
+    'multiply' => 'multiply',
+    'divide' => 'divide',
+    'modulo' => 'modulo'
+];
+
+// Handle form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $num1 = $_POST['number1'];
     $num2 = $_POST['number2'];
 
     if (is_numeric($num1) && is_numeric($num2)) {
-        if (isset($_POST['add'])) {
-            $result = $num1 + $num2;
-        } elseif (isset($_POST['subtract'])) {
-            $result = $num1 - $num2;
-        } elseif (isset($_POST['multiply'])) {
-            $result = $num1 * $num2;
-        } elseif (isset($_POST['divide'])) {
-            $result = $num2 != 0 ? $num1 / $num2 : "Cannot divide by 0";
-        } elseif (isset($_POST['modulo'])) {
-            $result = $num2 != 0 ? $num1 % $num2 : "Cannot modulo by 0";
+        foreach ($operations as $key => $function) {
+            if (isset($_POST[$key])) {
+                $result = $function($num1, $num2);
+                break;
+            }
         }
     } else {
-        $result = "Please enter numbers onli";
+        $result = "Please enter numbers only";
     }
 }
 ?>
 
 <h1>Simple Calculator</h1>
-
 <form method="post" action="">
     First Number:<br>
     <input type="text" name="number1" value="<?php if (isset($num1)) echo $num1; ?>"><br><br>
-
     Second Number:<br>
     <input type="text" name="number2" value="<?php if (isset($num2)) echo $num2; ?>"><br><br>
-
     <input type="submit" name="add" value="+">
     <input type="submit" name="subtract" value="-">
     <input type="submit" name="multiply" value="*">
